b6e06aa18a07cedc9ac1175bd4e0ca38e7a1bdef
Raúl González <rafex@rafex.dev>
2021-12-30 20:26:53 -0600

    fechas con el error

--- src/test/java/mx/rafex/bugs/DateTimeFormatterBugTest.java
+++ src/test/java/mx/rafex/bugs/DateTimeFormatterBugTest.java
@@ -8,28 +8,97 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class DateTimeFormatterBugTest {
-    
+
     private static final Logger LOGGER = Logger.getLogger(DateTimeFormatterBugTest.class.getName());
-    
+
     private static IDateTimeFormatterBug bug;
-    
+
     @BeforeAll
-    static void init(){
+    static void init() {
         bug = new DateTimeFormatterBug();
     }
-    
+
     @Test
-     void replyBug() {
-        String date = "2021-12-26";
-        String result = "20211226";
-        
-        
-        LocalDate localDate = bug.validDate(date);
-        
+    void replyBug20211226() {
+        final String date = "2021-12-26";
+        final String result = "20211226";
+
+        final LocalDate localDate = bug.validDate(date);
+
         LOGGER.info(date);
         LOGGER.info(result);
-        
-        Assertions.assertEquals(result,localDate.format(IDateTimeFormatterBug.DATE_FORMATTER));
-        
+
+        Assertions.assertEquals(result, localDate.format(IDateTimeFormatterBug.DATE_FORMATTER));
+
+    }
+
+    @Test
+    void replyBug20211227() {
+        final String date = "2021-12-27";
+        final String result = "20211227";
+
+        final LocalDate localDate = bug.validDate(date);
+
+        LOGGER.info(date);
+        LOGGER.info(result);
+
+        Assertions.assertEquals(result, localDate.format(IDateTimeFormatterBug.DATE_FORMATTER));
+
+    }
+
+    @Test
+    void replyBug20211228() {
+        final String date = "2021-12-28";
+        final String result = "20211228";
+
+        final LocalDate localDate = bug.validDate(date);
+
+        LOGGER.info(date);
+        LOGGER.info(result);
+
+        Assertions.assertEquals(result, localDate.format(IDateTimeFormatterBug.DATE_FORMATTER));
+
+    }
+
+    @Test
+    void replyBug20211229() {
+        final String date = "2021-12-29";
+        final String result = "20211229";
+
+        final LocalDate localDate = bug.validDate(date);
+
+        LOGGER.info(date);
+        LOGGER.info(result);
+
+        Assertions.assertEquals(result, localDate.format(IDateTimeFormatterBug.DATE_FORMATTER));
+
+    }
+
+    @Test
+    void replyBug20211230() {
+        final String date = "2021-12-30";
+        final String result = "20211230";
+
+        final LocalDate localDate = bug.validDate(date);
+
+        LOGGER.info(date);
+        LOGGER.info(result);
+
+        Assertions.assertEquals(result, localDate.format(IDateTimeFormatterBug.DATE_FORMATTER));
+
+    }
+
+    @Test
+    void replyBug20211231() {
+        final String date = "2021-12-31";
+        final String result = "20211231";
+
+        final LocalDate localDate = bug.validDate(date);
+
+        LOGGER.info(date);
+        LOGGER.info(result);
+
+        Assertions.assertEquals(result, localDate.format(IDateTimeFormatterBug.DATE_FORMATTER));
+
     }
 }
